6a9d0522951d85ec976ad0db3c0f728d68c9dd62
pulkitjo <pulkit.joshi@deliveryhero.com>
2022-05-30 19:19:56 +0200

    PDVG-7-Queryparams Implementation

--- src/main/java/com/deliveryhero/gateway/persistence/MinimalVendor.java
+++ src/main/java/com/deliveryhero/gateway/persistence/MinimalVendor.java
@@ -1,5 +1,7 @@
 package com.deliveryhero.gateway.persistence;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,15 +13,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Entity
-@Cacheable
-@Table(name = "minimal_vendor_tbl")
-public class MinimalVendor extends PanacheEntityBase {
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class MinimalVendor  {
 
-    @Id
+    @EqualsAndHashCode.Include
     public String bid;
 
-    private static List<MinimalVendor> mvs;
 
     public String topic;
 
@@ -27,33 +26,19 @@ public class MinimalVendor extends PanacheEntityBase {
 
     public String country;
 
-    public Date timestamp;
+    public String timestamp;
 
     public String version;
 
     public String payload;
 
-    public Date processed_at;
+    public String processed_at;
+
+    public String partition_key;
 
     public String protobuf;
 
 
-    public static List<MinimalVendor> getVendorByBID(Set<String> bids){
-        List<MinimalVendor> res=null;
-
-        if(mvs==null){
-            try(Stream<MinimalVendor> stream =MinimalVendor.streamAll()){
-                mvs = stream.collect(Collectors.toList());
-            }
-
-        }
-
-        res = mvs.stream().filter(mv -> bids.contains(mv.bid)).collect(Collectors.toList());
-
-
-        return res;
-
-    }
 
 
 
--- src/main/java/com/deliveryhero/gateway/rest/services/ListingService.java
+++ src/main/java/com/deliveryhero/gateway/rest/services/ListingService.java
@@ -6,24 +6,27 @@ import com.deliveryhero.gateway.rest.models.listing.AvailableVendorsResponse;
 import com.deliveryhero.gateway.rest.models.listing.MinimalVendorResponse;
 import com.deliveryhero.gateway.rest.services.clients.ListingClient;
 import com.deliveryhero.gateway.rest.services.clients.filters.QueryParams;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RequestScoped
 public class ListingService extends AbstractService {
-
-
-    @Inject
-    MinimalVendorResponse minimalVendorResponse;
 
 
 
@@ -33,18 +36,33 @@ public class ListingService extends AbstractService {
     @Inject
     QueryParams queryParams;
 
+    Set<MinimalVendor> mv=null;
+
+    Logger logger = LoggerFactory.getLogger(ListingService.class);
+
 
     @CircuitBreaker
     public Uni<MinimalVendorResponse> getMinimalVendors(AvailabilityResponse availabilityResponse){
+        long startTime =System.nanoTime();
+        MinimalVendorResponse response =new MinimalVendorResponse();
+        mapper = JsonMapper.builder().findAndAddModules().build();
+
+        try {
+            if(mv==null)
+            mv = Set.of(mapper.readValue(getClass().getResourceAsStream("/minimal_vendor_tbl.json"), MinimalVendor[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       Set<String> availabilitySet =availabilityResponse.getVendors().stream().map(v -> v.getId()).collect(Collectors.toSet());
+        response.setMv(
+                mv.stream().filter(e -> availabilitySet.contains(e.bid)).collect(Collectors.toList()));
+        logger.info("Minimal Vendor time : " + TimeUnit.MILLISECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
+        return Uni.createFrom().item(response);
 
 
 
-        minimalVendorResponse.setMv(
-                MinimalVendor.getVendorByBID(availabilityResponse.getVendors().stream().
-                map(vendor -> vendor.getId()).collect(Collectors.toSet())));
-
-
-        return Uni.createFrom().item(minimalVendorResponse);
+       // return Uni.createFrom().item(minimalVendorResponse);
     }
 
 
